ca6d66c7c024fac26a0fcd62149e51358d22ae17
martingh15 <martinghiotti@yahoo.com.ar>
2021-06-04 17:55:05 -0300

    Api rest productos

--- backend/urls.py
+++ backend/urls.py
@@ -6,4 +6,5 @@ app_name = "backend"
 
 urlpatterns = [
     path("productos/", views.listar_productos, name="productos"),
+    path('productos/<int:id>', views.ver_producto, name="ver_producto")
 ]
--- backend/views.py
+++ backend/views.py
@@ -1,24 +1,48 @@
-import re
-from django.http.response import JsonResponse
+from django.http.response import JsonResponse, HttpResponse
 from backend.models import Producto
-from .serializers import ProductoSerializer
+from .serializers import ProductoModalSerializer
 from rest_framework.parsers import JSONParser
+from django.views.decorators.csrf import csrf_exempt
 
 
-def index(request):
-    pass
-
-
+@csrf_exempt
 def listar_productos(request):
+
     if request.method == "GET":
         productos = Producto.objects.all()
-        serializer = ProductoSerializer(productos, many=True)
+        serializer = ProductoModalSerializer(productos, many=True)
         return JsonResponse(serializer.data, safe=False)
+
     elif request.method == "POST":
         data = JSONParser().parse(request)
-        serializer = ProductoSerializer(data=data)
+        serializer = ProductoModalSerializer(data=data)
 
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def ver_producto(request, id):
+    try:
+        producto = Producto.objects.get(pk=id)
+    except Producto.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == "GET":
+        serializer = ProductoModalSerializer(producto)
+        return JsonResponse(serializer.data)
+
+    if request.method == "PUT":
+        data = JSONParser().parse(request)
+        serializer = ProductoModalSerializer(producto, data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == "DELETE":
+        producto.delete()
+        return HttpResponse(status=204)
