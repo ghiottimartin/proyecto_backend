aab3d3710bc1c5797f60480034c7323bfd3d4a74
martingh15 <martinghiotti@yahoo.com.ar>
2021-06-05 14:51:53 -0300

    Usuario: campos de reset de password y activación

--- backend/models.py
+++ backend/models.py
@@ -4,7 +4,10 @@ from django.contrib.auth.models import AbstractUser
 
 class Usuario(AbstractUser):
     email = models.EmailField(unique=True)
-    
+    token_reset = models.CharField(max_length=191, null=True)
+    token_email = models.CharField(max_length=191, null=True)
+    fecha_token_reset = models.DateTimeField(null=True)
+
 class Producto(models.Model):
     nombre = models.CharField(max_length=30)
     precio = models.FloatField()
