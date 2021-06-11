62d345f3181505545434f76fba55bb8881459a14
martingh15 <martinghiotti@yahoo.com.ar>
2021-06-11 18:01:37 -0300

    Agrego auditoria a los productos y categorias

--- producto/apps.py
+++ producto/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
+from django.db.models.signals import pre_save
+from backend.signals import agregar_auditorias
 
 
 class ProductoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'producto'
+
+    # registering signals with the model's string label
+    pre_save.connect(agregar_auditorias, sender='producto.Categoria')
+    pre_save.connect(agregar_auditorias, sender='producto.Producto')
