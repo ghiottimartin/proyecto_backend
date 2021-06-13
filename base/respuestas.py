43f4db27a638ae9e648028451c4d0da164373222
martingh15 <martinghiotti@yahoo.com.ar>
2021-06-13 19:30:02 -0300

    Comentarios en base/respuestas.py

--- base/respuestas.py
+++ base/respuestas.py
@@ -2,6 +2,9 @@ from rest_framework.response import Response
 from rest_framework import status
 
 
+# Devuelve una respuesta lista para ser recibida por el frontend. Indicando un valor booleano en la clave 'exito' y un
+# mensaje en la clave 'message'. Dependiendo del éxito devuelve el código HTTP correspondiente pudiendo indicar uno
+# personalizado por el parámetro 'codigo'. Si deseamos devolver datos podemos hacerlo mediante el parámetro 'datos'.
 def get_respuesta(exito, mensaje="", codigo=None, datos=None):
     if exito and codigo is None:
         codigo = status.HTTP_200_OK
@@ -22,10 +25,12 @@ def get_respuesta(exito, mensaje="", codigo=None, datos=None):
     return Response(respuesta, status=codigo)
 
 
+# Respuesta exitosa por defecto sin mensaje ni datos.
 def exito():
     return get_respuesta(True)
 
 
+# # Respuestas de la acción validar token
 def validar_token_email_error_token_invalido():
     mensaje = "El token ingresado no es válido o ha caducado. Comuníquese con nosotros mediante la sección."
     return get_respuesta(False, mensaje)
@@ -36,6 +41,7 @@ def validar_token_email_error_general():
     return get_respuesta(False, mensaje)
 
 
+# # Respuestas de la acción olvido contraseña.
 def olvido_password_error_email_inexistente():
     mensaje = "El email ingresado no corresponde a ningún usuario registrado."
     return get_respuesta(False, mensaje)
@@ -51,6 +57,7 @@ def olvido_password_exito():
     return get_respuesta(True, mensaje)
 
 
+# # Respuestas de la acción validar token de cambio de contraseña.
 def validar_token_password_error_general():
     mensaje = "Hubo un error intentar validar el link. Intente de nuevo más tarde."
     return get_respuesta(False, mensaje)
@@ -61,6 +68,7 @@ def validar_token_password_error_link_invalido():
     return get_respuesta(False, mensaje)
 
 
+# # Respuestas de la acción cambiar contraseña.
 def cambiar_password_error_general():
     mensaje = "Hubo un error cambiar la contraseña. Intente de nuevo más tarde."
     return get_respuesta(False, mensaje)
