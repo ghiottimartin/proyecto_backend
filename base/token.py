3edd0b42d1df8e33c58b5eac073ece3698607b5f
martingh15 <martinghiotti@yahoo.com.ar>
2021-10-13 18:25:58 -0300

    Login: valido si el usuario fue borrado

--- base/token.py
+++ base/token.py
@@ -1,6 +1,9 @@
+from base.respuestas import Respuesta
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
+
+respuesta = Respuesta()
 
 
 class CustomAuthToken(ObtainAuthToken):
@@ -11,6 +14,8 @@ class CustomAuthToken(ObtainAuthToken):
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
         token, created = Token.objects.get_or_create(user=user)
+        if user.borrado:
+            return respuesta.get_respuesta(False, "El usuario ha sido borrado, contacte un administrador")
         return Response({
             'token': token.key,
             'idUsuario': user.pk,
